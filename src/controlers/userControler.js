fb35887b97694d5b36b2675f3e3b779cf024b464
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-03-24 17:34:04 +0300

    fix: response with all names when there is no q in params , trim and tolowercase relates #3

--- src/controlers/userControler.js
+++ src/controlers/userControler.js
@@ -4,15 +4,15 @@ const path = require("path");
 
 exports.getAutoComplete = async (request, response, next) => {
   const requestUrl = new URL(`http://localhost:300${request.url}`);
-  const queryAutoComplete = requestUrl.searchParams.get("q");
-  console.log(queryAutoComplete);
-  if (!queryAutoComplete) return console.log("not found");
+  const queryAutoComplete = (requestUrl.searchParams.get("q") || "")
+    .toLowerCase()
+    .trim();
 
   // open names.json file
   const rawdata = fs.readFileSync(path.join("models", "names.json"));
   const names = JSON.parse(rawdata);
   const filteredNames = names.filter((name) =>
-    name.includes(queryAutoComplete)
+    name.toLowerCase().includes(queryAutoComplete)
   );
   response.writeHead(200);
   response.end(JSON.stringify({ names: filteredNames }));
