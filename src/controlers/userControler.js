ebee06abe39c09090e1180be0038b182bd91c6dd
Batrawi <batrawi2000forever@gmail.com>
2023-03-24 17:29:10 +0200

    fix auery problem closes #5

--- src/controlers/userControler.js
+++ src/controlers/userControler.js
@@ -1,8 +1,10 @@
 const means = require("../models/meanings.json");
-const getMeanings = (response, str) => {
-  const myNameMeaning = JSON.stringify(means.str);
+const getMeaning = (request, response) => {
+  const requestURL = new URL(`http://local:3000${request.url}`);
+  const query = requestURL.searchParams.get("name");
+  const myNameMeaning = JSON.stringify(means[query]);
   response.writeHead(200, { "Content-Type": "text/html" });
   response.end(myNameMeaning);
 };
 
-module.exports = { getMeanings };
+module.exports = { getMeaning };
